8e6591f2dfa2494c8f698bc1b5eb9b69f7c00f31
xcrescent <xcrescent@users.noreply.github.com>
2023-10-03 15:23:48 +0000

    deploy: 7369708b3aa5cde068a020bd7d3c691117817f01

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "367c90a2b0a6efe1670a788655a586fb",
-"/": "367c90a2b0a6efe1670a788655a586fb",
-"main.dart.js": "ffdcf4b81a135d21c3a35295baa8d505",
+"index.html": "f2157c29d6bdb4ec615a88852fbe5d56",
+"/": "f2157c29d6bdb4ec615a88852fbe5d56",
+"main.dart.js": "54a61f37a058a7a353618bb9d273f7fa",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "987fa6283b96c91bc990da9e843a7c73"};
