fde89aa83b830295b185c2191434ed88561c4c2e
Utkarsh Singh <usjadon19@gmail.com>
2023-10-04 21:49:59 +0530

    Production level setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:portfolio_brl_trainees/app/app.dart';
 
-import 'package:portfolio_brl_trainees/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
