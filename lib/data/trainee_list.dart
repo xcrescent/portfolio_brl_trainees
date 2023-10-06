28145e0e09f9dcf14b6ad51edd9aaaab19a05c77
Gaurav Yadav <Gaurav88689548@gmail.com>
2023-10-06 18:52:03 +0530

    gaurav 1st commit for Hacktober Fest

--- lib/data/trainee_list.dart
+++ lib/data/trainee_list.dart
@@ -6,12 +6,12 @@ List<BrlCard> trainees = [
   const BrlCard(
     img:
         'https://xcrescent.github.io/portfolio_brl_trainees/assets/assets/images/usererror.png',
-    name: 'NAME',
+    name: 'Gaurav ',
     memberType: 'BRL Trainee',
     domain: 'App Developer',
-    git: 'https://github.com/xcrescent',
-    linked: 'https://www.linkedin.com/in/utkarsh-singh-jadon/',
-    ig: 'https://www.instagram.com/fkjrodnd/',
+    git: 'https://github.com/gauravydv007',
+    linked: 'https://www.linkedin.com/in/gaurav-singh-b710a1291?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    ig: 'https://www.instagram.com/gauravsingh_yaduvanshi',
   ),
   // Add your own card here
 ];
